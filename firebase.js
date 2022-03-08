d57b634f2a1b90bfdd2e049b2f6bb7a138083a87
Zakeu Aguiar <61065929+zakeeu@users.noreply.github.com>
2022-03-08 08:57:24 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = { 
+  apiKey : "AIzaSyDnES2rCG1Y0Fc0FWPxUk4I6kiC49Cqxuo" , 
+  authDomain : "to-do-live-8bd64.firebaseapp.com" , 
+  projectId : "to-do-live-8bd64" , 
+  storageBucket : "to-do-live-8bd64.appspot.com" , 
+  messagingSenderId : "853292215323" , 
+  appId : "1:853292215323:web:715e14d7a444cd0734dcbe" 
+};
+
+firebase.initializeApp(firebaseConfig);
+var db = initializeApp ( firebaseConfig );
